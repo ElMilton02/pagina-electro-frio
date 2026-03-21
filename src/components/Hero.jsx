baf8a848a575c089cd009d9bd423ec74f2169a9a
Milton Palucci <miltonpalucci02@gmail.com>
2026-03-20 22:50:41 -0300

    estructura base

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section>
+       <h2 className="hero__titulo">Reparacion e instalacion de todo tipo</h2>
+      <p className="hero__descripcion">Instalación, mantenimiento y reparación de equipos.</p>
+    </section>
+  )
+}
+
+export default Hero
